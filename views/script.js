d1b597d84fa1dafc946b130ad9a575b4fd945f35
mr-harshtyagi <writetoharshtyagi@gmail.com>
2022-12-23 14:16:07 +0530

    add content and styling

--- views/script.js
+++ views/script.js
@@ -1,21 +1,65 @@
-const alertPlaceholder = document.getElementById("liveAlertPlaceholder");
+// google charts
+// ACCELEROMETER CHART
+google.charts.load("current", { packages: ["corechart"] });
+google.charts.setOnLoadCallback(drawAccelerometerChart);
+google.charts.setOnLoadCallback(drawTemperatureChart);
 
-const alert = (message, type) => {
-  const wrapper = document.createElement("div");
-  wrapper.innerHTML = [
-    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
-    `   <div>${message}</div>`,
-    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-    "</div>",
-  ].join("");
+function drawAccelerometerChart() {
+  var data = google.visualization.arrayToDataTable([
+    ["Time (s)", "Vibration Amplitude (m/s^2)"],
+    ["1", 0],
+    ["2", 3],
+    ["3", 6],
+    ["4", 3],
+    ["5", 0],
+    ["6", -3],
+    ["7", -6],
+    ["8", -3],
+    ["9", 0],
+    ["10", 3],
+  ]);
 
-  alertPlaceholder.append(wrapper);
-};
+  var options = {
+    title: "Accelerometer Data",
+    curveType: "function",
+    legend: { position: "bottom" },
+  };
 
-const alertTrigger = document.getElementById("liveAlertBtn");
-if (alertTrigger) {
-  alertTrigger.addEventListener("click", () => {
-    console.log("clicked");
-    alert("Nice, you triggered this alert message!", "success");
-  });
+  var chart = new google.visualization.LineChart(
+    document.getElementById("accelerometer_chart_div")
+  );
+
+  chart.draw(data, options);
 }
+
+// TEMPERATURE SENSOR CHART
+function drawTemperatureChart() {
+  var data = google.visualization.arrayToDataTable([
+    ["Time (s)", "Temperature (°C)"],
+    ["1", 0],
+    ["2", 30],
+    ["3", 60],
+    ["4", 65],
+    ["5", 70],
+    ["6", 72],
+    ["7", 68],
+    ["8", 64],
+    ["9", 60],
+    ["10", 55],
+  ]);
+  var options = {
+    title: "Temperature Data",
+    curveType: "function",
+    legend: { position: "bottom" },
+  };
+
+  var chart = new google.visualization.LineChart(
+    document.getElementById("temperature_chart_div")
+  );
+
+  chart.draw(data, options);
+}
+
+// Temperrature value
+const temperatureValue = document.getElementById("temperature");
+temperatureValue.innerHTML = "20";
